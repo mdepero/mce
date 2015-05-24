ae35d4a47b38296ebcef386ffc485db7ad649bcb
Matt DePero <deperomd@gmail.com>
2015-05-24 12:53:51 -0400

    Work on faculty form

--- resources/scripts.js
+++ resources/scripts.js
@@ -25,6 +25,7 @@ $(document).ready(function(){
    *+-------------------------------------+
    */
   var faculty = [ "Bob", "Bill", "Brock", "Beth" ];
+  var semesters = [ "Fall 2001", "Spring 2001", "Fall 2002", "Spring 2002" ];
 
 
 
@@ -34,6 +35,12 @@ $(document).ready(function(){
   // Add current faculty to the list of faculty
   $.each(faculty, function(index, text) {
     $('#faculty').append( new Option(text,text) );
+
+  });
+
+  // Add available semesters
+  $.each(semesters, function(index, text) {
+    $('#semesters').append( new Option(text,text) );
 
   });
 
