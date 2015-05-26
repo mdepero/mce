30ed7b133901c468bb959a20fbbf55881e08b8d1
Matt DePero <deperomd@gmail.com>
2015-05-25 20:04:42 -0400

    Finished first draft on forms

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,8 +1,38 @@
 // All Code by Matt DePero Unless Noted
 
 
-// Smooth Scroll Code provided by CSS Tricks https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+  /*+-------------------------------------+
+   *|   To be retreived from database     |
+   *|    End result array for testing     |
+   *+-------------------------------------+
+   */
+  var faculty = [ "Bob", "Bill", "Barry", "Beth" ];
+  var semesters = [ "Fall 2001", "Spring 2001", "Fall 2002", "Spring 2002" ];
+  var students = {
+      1001: "Matt",
+      1101: "Todd",
+      1002: "Ed",
+      1003: "Norm",
+      1004: "Mike",
+      1104: "Joe",
+      1105: "Jill",
+      1005: "Samantha",
+      1006: "Steven",
+      1007: "Marco",
+      1008: "Elaine",
+      1009: "Kate",
+      1010: "Hailey"
+    };
+  var facultyToStudents = [ [1001,1002,1101], [1003,1004,1104,1105], [1005,1006,1009], [1007,1008,1010] ];
+
+
+
+
+
 $(document).ready(function(){
+
+  // Smooth Scroll Code provided by CSS Tricks https://css-tricks.com/snippets/jquery/smooth-scrolling/
   $('a[href*=#]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
     && location.hostname == this.hostname) {
@@ -19,46 +49,18 @@ $(document).ready(function(){
   });
 
 
-  /*+-------------------------------------+
-   *|   To be retreived from database     |
-   *|    End result array for testing     |
-   *+-------------------------------------+
-   */
-  var faculty = [ "Bob", "Bill", "Barry", "Beth" ];
-  var semesters = [ "Fall 2001", "Spring 2001", "Fall 2002", "Spring 2002" ];
-  var students = {
-    Bob:{
-      1001: "Moe",
-      1002: "Moro"
-    },
-    Bill:{
-      1003: "Milly",
-      1004: "Miles"
-    },
-    Barry:{
-      1005: "Matt",
-      1006: "Mary"
-    },
-    Beth:{
-      1007: "Meredeth",
-      1008: "Me"
-    }
-  }
-
-
-
 
 
 
   // Add current faculty to the list of faculty
   $.each(faculty, function(index, text) {
-    $('#faculty').append( new Option(text,text) );
+    $('#faculty').append( new Option(text,index) );
 
   });
 
   // Add available semesters
   $.each(semesters, function(index, text) {
-    $('#semesters').append( new Option(text,text) );
+    $('#semester').append( new Option(text,text) );
 
   });
 
@@ -72,12 +74,40 @@ $(document).ready(function(){
 
 function addStudents(){
 
-  $.each(students[$("#faculty").val()], function(index, text) {
-    $('#students').append( "<tr><td>" + text + "</td></tr>" );
+  $("#error").html("");
+  if($("#faculty").val() == "" || $("#semester").val() == ""){
+    $("#error").html("You are missing something on the current form.");
+    return;
+  }
+
+  $.each(facultyToStudents[$("#faculty").val()], function(index, text) {
+    $('#students').append( '<input type="checkbox" name="students" value="' + text + '" id="' + text + '">&nbsp;<label for="' + text + '"">' + students[text] + "</label><br/>" );
 
   });
 
-  $("#1").fadeIn(2000);
+  $("#addStudents").prop("disabled",true);
+
+  $("#0").fadeOut(1000, "swing", function(){
+    $("#1").fadeIn(1000);
+  });
+
+}
 
 
-  }
+function addForms(){
+
+  $.each( $('input[name="students"]:checked'), function(index, text) {
+
+    $("#forms").append(
+        "<p><h2>" + students[$(text).val()] + "</h2>"+'<input type="hidden" name="id[]" value="'+$(text).val()+'"'+"<p>THIS WILL BE THIS PERSONS FORM TO FILL OUT</p></p>"
+      );
+
+
+
+  $("#1").fadeOut(1000, "swing", function(){
+    $("#2").fadeIn(1000);
+  });
+
+  });
+
+}
