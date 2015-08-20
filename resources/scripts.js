c321013bdba2ac7de44820f605c3e3932c67fc11
= <=>
2015-08-19 21:55:44 -0400

    work on admin stuff

--- resources/scripts.js
+++ resources/scripts.js
@@ -721,27 +721,34 @@ function setClass(){
 }
 
 
-
+var fileUploadData;
 
 function setStudents(){
-  var f = $('#studentData').target.files[0]; 
 
-  if (f) {
-    var r = new FileReader();
-    r.onload = function(e) { 
-      var contents = e.target.result;
-      alert( "Got the file.n" 
-            +"name: " + f.name + "n"
-            +"type: " + f.type + "n"
-            +"size: " + f.size + " bytesn"
-            + "starts with: " + contents
-      );  
-    }
-    r.readAsText(f);
-  } else { 
-    alert("Failed to load file");
-  }
 }
+
+function readSingleFile(evt) {
+    //Retrieve the first (and only!) File from the FileList object
+    var f = evt.target.files[0]; 
+
+    if (f) {
+      var r = new FileReader();
+      r.onload = function(e) { 
+        fileUploadData = e.target.result;
+        alert( "Got the file.n" 
+              +"name: " + f.name + "n"
+              +"type: " + f.type + "n"
+              +"size: " + f.size + " bytesn"
+              + "starts with: " + contents.substr(1, contents.indexOf("n"))
+        );  
+      }
+      r.readAsText(f);
+    } else { 
+      alert("Failed to load file");
+    }
+  }
+
+  document.getElementById('studentData').addEventListener('change', readSingleFile, false);
 
 
 
